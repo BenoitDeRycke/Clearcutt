d4a099378ea9925f4712411112f946ee37ffd0a9
Benoît De Rycke <benoit.derycke@student.hogent.be>
2025-05-09 15:49:12 +0200

    type fout in .env

--- backend/api/supabaseClient.js
+++ backend/api/supabaseClient.js
@@ -1,8 +1,8 @@
 const { createClient } = require("@supabase/supabase-js");
 
 const SUPABASE_URL = process.env.SUPABASE_URL;
-const SUPABASE_SERVIEC_ROL_KEY = process.env.SUPABASE_SERVIEC_ROL_KEY;
+const SUPABASE_SERVICE_ROL_KEY = process.env.SUPABASE_SERVICE_ROL_KEY;
 
-const supabase = createClient(SUPABASE_URL, SUPABASE_SERVIEC_ROL_KEY);
+const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROL_KEY);
 
 module.exports = supabase;
